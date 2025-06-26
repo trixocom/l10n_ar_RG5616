be5c480875c3a5004918b932d80625e6309440d1
TRX <hectorquiroz@trixocom.com>
2025-06-26 17:38:37 -0300

    Add files via upload

---
+++ l10n_ar_rg5616/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "l10n_ar RG 5616 ",
+    "version": "15.9.0.0.0",
+    "summary": """ 
+                   RG 5616/24. identificación fiscal de los clientes frente al IVA durante la fiscalización de facturas en ARCA.
+  """,
+    "author": "Walter Donda",
+    "website": "",
+    "category": "Localizacion",
+    "depends": ["account", "l10n_ar_afipws_fe"],
+    "data": [
+        
+    ],
+    "assets": {
+        "web.assets_backend": [],
+        "web.assets_qweb": [],
+    },
+    "application": True,
+    "installable": True,
+    "auto_install": False,
+    "license": "LGPL-3",
+}
